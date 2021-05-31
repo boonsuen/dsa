c84e506b72dbaa1edc79dd119b6c4d793302ce5d
Boonsuen Oh <boonsuenoh@gmail.com>
2021-05-31 22:31:00 +0800

    fix: typings

--- src/ui/ButtonLink.tsx
+++ src/ui/ButtonLink.tsx
@@ -1,41 +1,46 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx } from "theme-ui"
-import React from "react"
-import PropTypes from "prop-types"
-import Link from "next/link"
+import { jsx } from 'theme-ui';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
 
-const A = React.forwardRef(({ onClick, href, styles, children }, ref) => {
-  return (
-    <a href={href} sx={styles} ref={ref}>
-      {children}
-    </a>
-  )
-})
+const A = React.forwardRef(
+  (
+    { href, styles, children }: { href?: any; styles: any; children: any },
+    ref: any
+  ) => {
+    return (
+      <a href={href} style={styles} ref={ref}>
+        {children}
+      </a>
+    );
+  }
+);
 
 const ButtonLink = (props) => {
-  let py = 2
-  if (typeof props.fontSize === "number") {
-    py = props.fontSize - 1
+  let py = 2;
+  if (typeof props.fontSize === 'number') {
+    py = props.fontSize - 1;
   } else if (props.fontSize.length > 1) {
-    py = props.fontSize.map((n) => n - 1)
+    py = props.fontSize.map((n) => n - 1);
   }
 
   const styles = {
-    textDecoration: "none",
-    fontWeight: props.fontWeight || "normal",
+    textDecoration: 'none',
+    fontWeight: props.fontWeight || 'normal',
     fontSize: props.fontSize || 3,
     px: props.fontSize || 3,
     py,
-    bg: props.bg || "primary",
-    color: props.bg === "white" ? "primary" : "#fff",
-    boxShadow: "none",
-    borderRadius: "8px",
-    border: "none",
-    cursor: "pointer",
-    display: "inline-block",
-  }
-  return props.href.includes("http") ? (
+    bg: props.bg || 'primary',
+    color: props.bg === 'white' ? 'primary' : '#fff',
+    boxShadow: 'none',
+    borderRadius: '8px',
+    border: 'none',
+    cursor: 'pointer',
+    display: 'inline-block',
+  };
+  return props.href.includes('http') ? (
     <A href={props.href} styles={styles}>
       {props.children}
     </A>
@@ -43,7 +48,7 @@ const ButtonLink = (props) => {
     <Link href={props.href} passHref>
       <A styles={styles}>{props.children}</A>
     </Link>
-  )
-}
+  );
+};
 
-export default ButtonLink
+export default ButtonLink;
