dd0862b86bf4f2bc9799f314df12a397155559d1
Boonsuen Oh <boonsuenoh@gmail.com>
2021-06-04 13:27:59 +0800

    refactor: rename Math component to InlineMath

--- src/ui/Math.tsx
+++ src/ui/Math.tsx
@@ -1,10 +1,4 @@
 import 'katex/dist/katex.min.css';
 import { InlineMath, BlockMath } from 'react-katex';
 
-const KatexMath = (props) => {
-  return (
-    <InlineMath {...props} />
-  );
-};
-
-export default KatexMath;
+export { InlineMath };
