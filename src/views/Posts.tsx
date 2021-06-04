9e8c14f12607bb83cf91203e338d14cdcb4db796
Boonsuen Oh <boonsuenoh@gmail.com>
2021-06-04 13:59:51 +0800

    style: table

--- src/views/Posts.tsx
+++ src/views/Posts.tsx
@@ -1,18 +1,58 @@
 import MDX from '@mdx-js/runtime';
+import styled from 'styled-components';
 import { Flex, Box, Heading, Text } from 'theme-ui';
 import Container from '../ui/Container';
 import DraftBadge from '../ui/DraftBadge';
 import Link from 'next/link';
+
+const StyledTable = styled.table`
+  border-collapse: collapse;
+  margin: 1.25em 0 0;
+  width: 100%;
+
+  th, td {
+    padding: 10px 12px;
+    text-align: left;
+  }
+
+  tr td {
+    background-color: #f8f8f8;
+  }
+
+  tr:nth-of-type(2n + 2) td {
+    background-color: #fff;
+  }
+`;
+
+interface DifficultyProps {
+  readonly difficulty: 'Easy' | 'Medium' | 'Hard';
+}
+
+const Difficulty = styled.code<DifficultyProps>`
+  background-color: ${(props) => {
+    if (props.difficulty === 'Easy') {
+      return '#5fb760';
+    } else if (props.difficulty === 'Medium') {
+      return '#eeac57';
+    } else {
+      return '#d75452';
+    }
+  }};
+  color: #fff;
+  padding: 4px 8px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 14px;
+  border-radius: 20px;
+`;
 
 const Posts = ({ posts, prevPosts, nextPosts }) => {
   const isLocal = process.env.NODE_ENV === 'development';
 
   return (
     <Container>
-      <MDX>
-        # Problems
-      </MDX>
-      <table id="table" className="leetcode-prob">
+      <MDX># Problems</MDX>
+      <StyledTable>
         <thead>
           <tr>
             <th>#</th>
@@ -22,31 +62,30 @@ const Posts = ({ posts, prevPosts, nextPosts }) => {
           </tr>
         </thead>
         <tbody>
-          {
-            posts && posts.filter((post) => {
-              return isLocal || !post.draft;
-            }).map((post, i) => (
-              <tr key={`tr-${post.slug}`}>
-                <td>{post.number}</td>
-                <td style={{position: 'relative'}}>
-                  {post.draft && <DraftBadge />}
-                  <Link href={'/' + post.slug} passHref>
-                    <a>{post.title}</a>
-                  </Link>
-                </td>
-                <td>
-                  <code className="button medium">
-                    {post.difficulty}
-                  </code>
-                </td>
-                <td>
-                  {post.topic}
-                </td>
-              </tr>
-            ))
-          }
+          {posts &&
+            posts
+              .filter((post) => {
+                return isLocal || !post.draft;
+              })
+              .map((post, i) => (
+                <tr key={`tr-${post.slug}`}>
+                  <td>{post.number}</td>
+                  <td style={{ position: 'relative' }}>
+                    {post.draft && <DraftBadge />}
+                    <Link href={'/' + post.slug} passHref>
+                      <a>{post.title}</a>
+                    </Link>
+                  </td>
+                  <td>
+                    <Difficulty difficulty={post.difficulty}>
+                      {post.difficulty}
+                    </Difficulty>
+                  </td>
+                  <td>{post.topic}</td>
+                </tr>
+              ))}
         </tbody>
-      </table>
+      </StyledTable>
       <Flex sx={{ fontStyle: 'italic' }}>
         <Box sx={{ width: '50%', py: 3, textAlign: 'left' }}>
           {prevPosts !== null && (
