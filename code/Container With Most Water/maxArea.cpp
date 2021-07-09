9db41a428732fe19c65b498f1cbbedfc76cc84b1
Boonsuen Oh <boonsuenoh@gmail.com>
2021-07-09 17:49:33 +0800

    container-with-most-water cpp solution

---
+++ code/Container With Most Water/maxArea.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using std::vector;
+using std::min;
+using std::max;
+
+class Solution {
+  public:
+    int maxArea(vector<int>& height) {
+      int water = 0;
+      int i = 0, j = height.size() - 1;
+      while (i < j) {
+        int h = min(height[i], height[j]);
+        water = max(water, (j - i) * h);
+        while (height[i] <= h && i < j) i++;
+        while (height[j] <= h && i < j) j--;
+      }
+      return water;
+    }
+};
+
